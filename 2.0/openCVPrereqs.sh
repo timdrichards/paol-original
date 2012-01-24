c07e9bbf8a304b9a3ba288c86d9e7f42cc544bc7
David Warshow <diw08@bigred.(none)>
2012-01-24 11:55:09 -0500

    adding prereq bash for fred

---
+++ 2.0/openCVPrereqs.sh
@@ -0,0 +1,17 @@
+sudo apt-get install build-essential cmake
+sudo apt-get install pkg-config
+sudo apt-get install libpng12-0 libpng12-dev libpng++-dev libpng3
+sudo apt-get install libpnglite-dev libpngwriter0-dev libpngwriter0c2
+sudo apt-get install zlib1g-dbg zlib1g zlib1g-dev
+sudo apt-get install libjasper-dev libjasper-runtime libjasper1
+sudo apt-get install pngtools libtiff4-dev libtiff4 libtiffxx0c2 libtiff-tools
+sudo apt-get install libjpeg62-dev libgtk2.0-dev libswscale-dev
+sudo apt-get install libavcodec-dev libavcodec52 libavformat52 libavformat-dev
+sudo apt-get install libgstreamer0.10-0-dbg libgstreamer0.10-0  libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install libxine1-ffmpeg  libxine-dev libxine1-bin
+sudo apt-get install libunicap2 libunicap2-dev
+sudo apt-get install libdc1394-22-dev libdc1394-22 libdc1394-utils
+sudo apt-get install swig
+sudo apt-get install libv4l-0 libv4l-dev
+sudo apt-get install python-numpy
+sudo apt-get install libpython2.6 python-dev python2.6-dev
