f90b64bd6af687f1a8c3c006b1d567bb42eeea93
David Warshow <diw08@hampshire.edu>
2012-06-27 12:04:41 -0400

    Starting computer proc testing

--- main.cpp
+++ main.cpp
@@ -34,65 +34,25 @@
 
 using namespace cv;
 
+void computerPipeline()
+{
+
+
+}
+
 int main(int argc, char** argv)
 {
-  //Hack to avoid unused var warning
-  argc++;
-  argc--;
+  
   char outDir[1024];
   std::strcpy(outDir, argv[1]);
-  char WBdir[1024];
-  std::strcpy(WBdir, argv[2]);
-  char WBfirst[1024];
-  std::strcpy(WBfirst, argv[3]);
-  char WBpattern[512];
-  std::strcpy(WBpattern, argv[4]);
   
-  ////////////////////////////////
-  /// Camera/WB Cap/Proc Buffers /
-  Buffer* wbBuffer;
-  wbBuffer = new Buffer;
-  Buffer* lectVideoBuffer;
-  lectVideoBuffer = new Buffer;
-  Buffer* wbSlidesBuffer;
-  wbSlidesBuffer = new Buffer;
-  Buffer* wbSlidesWriteBuffer;
-  wbSlidesWriteBuffer = new Buffer;
-  Buffer* wbReadBuffer;
-  wbReadBuffer = new Buffer;
-
-  //////////////////////////////////////
-  // WB Cap/Proc Modules
-  //  WriteMod presVideoFramesWriter(lectVideoBuffer);
-  WriteMod wbSlidesWriter(wbSlidesWriteBuffer);
-  WhiteBoardFoot wbSlides(wbSlidesBuffer,wbSlidesWriteBuffer);
-  WhiteBoardProcess wbproc(wbBuffer, wbSlidesBuffer);
-  // LectVideoFrameCreate lectFrameCreator(wbBuffer, lectVideoBuffer);
-  LocateSpeaker locateSpeaker(wbReadBuffer, wbBuffer);
-  ReadMod readFromDisk(wbReadBuffer);
-
-  ///////////////////////////////////////
-  // Launch WB mods in revers order /////
+  char Cdir[1024];
+  std::strcpy(Cdir, argv[2]);
   
-  boost::thread_group wbThreads;
-
-  //wbThreads.create_thread(boost::bind(&WriteMod::WriteMatsByCount, &presVideoFramesWriter, outDir));
-  wbThreads.create_thread(boost::bind(&WriteMod::WriteMats, &wbSlidesWriter, outDir));
-  wbThreads.create_thread(boost::bind(&WhiteBoardFoot::run, &wbSlides));
-  wbThreads.create_thread(boost::bind(&WhiteBoardProcess::run, &wbproc, 1));
-  wbThreads.create_thread(boost::bind(&LocateSpeaker::run, &locateSpeaker));
-  wbThreads.create_thread(boost::bind(&ReadMod::ReadFromPatternFlipExt, &readFromDisk, WBdir,WBfirst,WBpattern));
+  char Cfirst[1024];
+  std::strcpy(Cfirst, argv[3]);
   
-  wbThreads.join_all();
   
-  std::cout<<"Main: about to begin deleting WB buffers"<<std::endl;
-  delete wbSlidesBuffer;
-  delete wbBuffer;
-  delete lectVideoBuffer;
-  //delete lectVideoBuffer;
-  delete wbReadBuffer;
-  std::cout<<"Main: finished deleting WB buffers"<<std::endl;
-
   return 0;
 }
 
---
+++ main_whiteBoard.cpp
@@ -0,0 +1,55 @@
+//Hack to avoid unused var warning
+argc++;
+argc--;
+
+char WBdir[1024];
+std::strcpy(WBdir, argv[2]);
+char WBfirst[1024];
+std::strcpy(WBfirst, argv[3]);
+char WBpattern[512];
+std::strcpy(WBpattern, argv[4]);
+
+////////////////////////////////
+/// Camera/WB Cap/Proc Buffers /
+Buffer* wbBuffer;
+wbBuffer = new Buffer;
+Buffer* lectVideoBuffer;
+lectVideoBuffer = new Buffer;
+Buffer* wbSlidesBuffer;
+wbSlidesBuffer = new Buffer;
+Buffer* wbSlidesWriteBuffer;
+wbSlidesWriteBuffer = new Buffer;
+Buffer* wbReadBuffer;
+wbReadBuffer = new Buffer;
+
+//////////////////////////////////////
+// WB Cap/Proc Modules
+//  WriteMod presVideoFramesWriter(lectVideoBuffer);
+WriteMod wbSlidesWriter(wbSlidesWriteBuffer);
+WhiteBoardFoot wbSlides(wbSlidesBuffer,wbSlidesWriteBuffer);
+WhiteBoardProcess wbproc(wbBuffer, wbSlidesBuffer);
+// LectVideoFrameCreate lectFrameCreator(wbBuffer, lectVideoBuffer);
+LocateSpeaker locateSpeaker(wbReadBuffer, wbBuffer);
+ReadMod readFromDisk(wbReadBuffer);
+
+///////////////////////////////////////
+// Launch WB mods in revers order /////
+  
+boost::thread_group wbThreads;
+
+//wbThreads.create_thread(boost::bind(&WriteMod::WriteMatsByCount, &presVideoFramesWriter, outDir));
+wbThreads.create_thread(boost::bind(&WriteMod::WriteMats, &wbSlidesWriter, outDir));
+wbThreads.create_thread(boost::bind(&WhiteBoardFoot::run, &wbSlides));
+wbThreads.create_thread(boost::bind(&WhiteBoardProcess::run, &wbproc, 1));
+wbThreads.create_thread(boost::bind(&LocateSpeaker::run, &locateSpeaker));
+wbThreads.create_thread(boost::bind(&ReadMod::ReadFromPatternFlipExt, &readFromDisk, WBdir,WBfirst,WBpattern));
+  
+wbThreads.join_all();
+  
+std::cout<<"Main: about to begin deleting WB buffers"<<std::endl;
+delete wbSlidesBuffer;
+delete wbBuffer;
+delete lectVideoBuffer;
+//delete lectVideoBuffer;
+delete wbReadBuffer;
+std::cout<<"Main: finished deleting WB buffers"<<std::endl;
